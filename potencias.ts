d77fde9ef844d15d02bd96de27ad1131c6cb1de3
fmjalvarez <168040644+fmjalvarez@users.noreply.github.com>
2024-05-08 18:36:18 -0300

    borre una variable que no se usaba

--- potencias.ts
+++ potencias.ts
@@ -2,7 +2,7 @@ import * as readlineSync from 'readline-sync';
 console.log ("Este programa calcula la potencia de un numero ingresano la base y el exponente ");
 let base : number = readlineSync.questionInt ("Ingrese el valor de la base: ");
 let exponente : number = readlineSync.questionInt ("Ingrese el valor del exponente: ");
-let i:number, resultado:number =0, acumulado:number = 0
+let i:number, resultado:number =0
 if (exponente==0){
     console.log ("1");
 }else{resultado=base
